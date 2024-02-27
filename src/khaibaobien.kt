f2ff2a091bd8306f9cad285744862e8829dcbc7e
vtlanh17 <volanhbi17@gmail.com>
2024-02-27 16:40:05 +0700

    cập nhật ngày đầu tiên

---
+++ src/khaibaobien.kt
@@ -0,0 +1,22 @@
+fun main(args: Array<String>) {
+    /*var khai báo biến cho phép thay đổi giá trị của biến
+    val không cho phép thay đổi giá trị của biến trong (hằng số)*/
+    /* khai báo biến/khởi tạo biến
+       var tên biến : kiểu dữ liệu [=giá trị] */
+    var soA : Short = 143
+    var soB = 147
+    println(soA ::class.java.typeName)
+    println(soB ::class.java.typeName)
+    //kiểu ký tự
+    var kytu: Char = 'a'
+    //khai báo bool
+    var check : Boolean
+    /*cú pháp khai báo mảng
+    var tên Arname : Artype = Artypeof(giá trị)*/
+    var mangSothuc : FloatArray = floatArrayOf(0.5f,7.5f,5f,14.3f)
+    //khai bao hằng
+    val nhietdosoi : Int = 100
+    val nhietdodong : Int = 0
+
+
+}
---
+++ src/xuat.kt
@@ -0,0 +1,7 @@
+fun main(args: Array<String>) {
+
+   println(" \t thut vao 1 tab") //xuong dong
+    println(" xuong dong \n ")
+   print("\"dau nay de trich dan\" ")//khong xuong dong
+
+}
